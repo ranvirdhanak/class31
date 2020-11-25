272cb11ff5b375bcb8adc286b2879fe24a0b1500
ranvir <ranvir.dhanak@gmail.com>
2020-11-25 14:56:33 +0530

    we created gameState

--- sketch.js
+++ sketch.js
@@ -1,3 +1,41 @@
+var string = "this is a string";
+console.log(string);
+
+var num = 100;
+console.log(num);
+
+var bool = true;
+console.log(bool);
+
+var object;
+console.log(object);
+
+object = null;
+console.log(object);
+
+var arr1 = [1,2,3,4,5];
+console.log(arr1);
+
+var arr2 = [1,"abc",false,null];
+console.log(arr2)
+
+var arr3 = [[1,2],[2,3],[3,4]]
+console.log(arr3)
+
+console.log(arr3[0])
+
+console.log(arr3[0][0])
+
+console.log(arr3[0][1])
+
+arr3.push("Ranvir");
+
+console.log(arr3);
+
+arr3.pop();
+
+console.log(arr3);
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,7 +45,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +107,23 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!== "launched"){
+    
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+  
+
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
+       // slingshot.attach(bird.body);
+ 
+   }
 }
